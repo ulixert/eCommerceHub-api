f7f2ab1ea3e08b34fa044e14571d9a1162a15508
matt <143686761+ulixert@users.noreply.github.com>
2024-11-10 14:57:30 -0800

    Initial commit

---
+++ src/main/java/com/ulixert/ecommercehub/EcommerceHubApplication.java
@@ -0,0 +1,13 @@
+package com.ulixert.ecommercehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ulixert/ecommercehub/EcommerceHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulixert.ecommercehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
